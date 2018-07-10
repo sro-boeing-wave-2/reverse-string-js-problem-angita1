a22ef6c0c0c6d3beb54078b390de4d80e0637941
Yaiphabee <ksh.yaipha@gmail.com>
2018-07-10 19:35:53 +0530

    fix error

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,15 +1,9 @@
 // The intent of this file is to reverse
 // the string using built in functions.
-const reverse = (str) => {
-  var i;
-  var result="";
-  for(i=str.length-1;i>=0;i-=1){
-      result+=str[i];
-  }
-  return result;
-};
-
+function reverse(str) {
+  const array = str.split('');
+  const reverseArray = array.reverse();
+  const joinArray = reverseArray.join('');
+  return joinArray;
+}
 module.exports = reverse;
- //Here the built-in function "split" separates all the characters in the 
-                                                  //string and the "reverse" function reverse all the characters
-                                                  //the reversed characters are then comblined using "join" funtion.
--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,9 +1,12 @@
 // The intent of this file is to reverse the string using iterators (for, foreach)
+
 const reverse = (str) => {
-    var reverseString=str.split("").reverse().join("");
-    return reverseString;
+  let i = str.length - 1;
+  let result = '';
+  for (i = str.length - 1; i >= 0; i -= 1) {
+    result += str[i];
+  }
+  return result;
 };
 
 module.exports = reverse;
-
-
